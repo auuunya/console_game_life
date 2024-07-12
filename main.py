b158b8f23b07bee55152a19bb2c0827e694373d9
zhangchenyang <zhangchenyang@seclover.com>
2024-07-12 09:06:42 +0800

    add curses

--- main.py
+++ main.py
@@ -7,7 +7,7 @@
 '''
 
 # here put the import lib
-import random, os, time, argparse
+import random, os, time, argparse, curses
 from functools import reduce
 
 
@@ -56,6 +56,25 @@ def render(states):
   print(bottom_border)
   print(f"{RED_COLOR}alive count: {alive_count}{RESET_COLOR}\n")
 
+def render_str(states):
+  alive_count = 0
+  render_content = ""
+  top_border = "+" + "-" * len(states[0]) + "+\n"
+  render_content += render_content
+  for row in states:
+      var_char = "|"
+      for item in row:
+          if item:
+              alive_count += 1
+              var_char += ALIVE_COLOR + "#" + RESET_COLOR
+          else:
+              var_char += DIED_COLOR + "." + RESET_COLOR
+      var_char += "|"
+      render_content = render_content + var_char + "\n"
+  bottom_border = "+" + "-" * len(states[0]) + "+\n"
+  render_content += bottom_border
+  return render_content, alive_count
+
 def next_board_state(init_state):
     lines = len(init_state)
     rows = len(init_state[0])
@@ -102,7 +121,6 @@ def print_arguments():
     parser = argparse.ArgumentParser(
         prog="Console Game Life",
         description="A console implementation of Conway's Game of Life.",
-        formatter_class=argparse.RawTextHelpFormatter,
     )
     parser.add_argument(
         "-f",
@@ -133,6 +151,85 @@ def print_arguments():
         args.time = 0.5
     return args
 
+
+help_messages = [
+    "Help:",
+    "Press 'Q' or 'q' to quit."
+]
+def draw_render(win, states):
+    alive_count = 0
+    moveY = 0
+    maxHeight, maxWidth = win.getmaxyx()
+    cols = len(states)
+    rows = len(states[0])
+    if rows + 2 > maxWidth:
+        states = random_state(maxWidth, cols)
+    if cols + 4 > maxHeight:
+        states = random_state(rows, maxHeight)
+
+    for help_message in help_messages:
+        win.addstr(moveY, 0, help_message)
+        moveY += 1
+    moveY = moveY + 1
+    top_border = "+" + "-" * len(states[0]) + "+"
+    win.attron(curses.color_pair(1))
+    win.addstr(moveY, 0, top_border)
+    win.attroff(curses.color_pair(1))
+    for row in states:
+        var_char = "|"
+        moveX = 1
+        moveY += 1
+        win.attron(curses.color_pair(1))
+        win.addstr(moveY, 0, "|")
+        win.attroff(curses.color_pair(1))
+        for item in row:
+            moveX += 1
+            if item:
+                alive_count += 1
+                var_char += "#" 
+                win.attron(curses.color_pair(2))
+                win.addstr(moveY, moveX, "#")
+                win.attroff(curses.color_pair(2))
+            else:
+                var_char += "."
+                win.attron(curses.color_pair(1))
+                win.addstr(moveY, moveX, ".")
+                win.attroff(curses.color_pair(1))
+        var_char += "|"
+        
+        win.attron(curses.color_pair(1))
+        win.addstr(moveY, len(row) + 1, "|")
+        win.attroff(curses.color_pair(1))
+        # win.addstr(moveY, 0, var_char)
+    bottom_border = "+" + "-" * len(init_states[0]) + "+"
+    win.attron(curses.color_pair(1))
+    win.addstr(moveY, 0, bottom_border)
+    win.attroff(curses.color_pair(1))
+    moveY += 1
+    win.addstr(moveY,0, f"Alive Count: {alive_count}")
+    win.refresh()
+
+def draw(init_states):
+    stdscr = curses.initscr()    
+    curses.curs_set(0)
+    stdscr.clear()
+    play_win = curses.newwin(0, 0, 0, 0)
+    # Setup colors
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    # Main loop
+    play_win.nodelay(True)
+    while True:
+        draw_render(play_win, init_states)
+        init_states = next_board_state(init_states)
+        play_win.refresh()
+        key = play_win.getch()
+        if key == ord('q'):
+            break
+        time.sleep(args.time)
+        
+
 if __name__ == '__main__':
     args = print_arguments()
     init_states = []
@@ -146,12 +243,5 @@ if __name__ == '__main__':
         init_states = random_state(rows, cols)
     else:
         args = print_arguments()
-
-    try:
-      while True:
-          new_state = next_board_state(init_states)
-          render(new_state)
-          init_states = new_state
-          time.sleep(args.time)
-    except KeyboardInterrupt:
-      print ("\nExiting console game life.")
+        
+    draw(init_states)
